15369328576404cdf8c999a76f4dc821b059e5d6
Carlos Breceda <CBreceda3@my.scccd.edu>
2025-09-02 20:34:25 -0700

    updated code after figuring out an error I had near the top of my app.js file.

--- w4/app.js
+++ w4/app.js
@@ -2,7 +2,7 @@
 // Get the form from index.html, add an event listener, make sure it getst he input, respond to the input or lack there of, reset on reload.
 document.addEventListener("DOMContentLoaded", function() {             
     const formElement = document.getElementById("generatorForm");   
-    const nameInput = document.getElementById("InputName");
+    const nameInput = document.getElementById("inputName"); //Had to correct incorrect capitalized I in input
     const messageElement = document.getElementById("greeting");
 
     formElement.addEventListener("submit", function(event) {           
