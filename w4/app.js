1a93c722455aaa737022c95eba7d5abf3abbd96b
Carlos Breceda <CBreceda3@my.scccd.edu>
2025-09-02 20:24:14 -0700

    feat: initial attempt at greeting generator

---
+++ w4/app.js
@@ -0,0 +1,21 @@
+//<-- Plan  -->//
+// Get the form from index.html, add an event listener, make sure it getst he input, respond to the input or lack there of, reset on reload.
+document.addEventListener("DOMContentLoaded", function() {             
+    const formElement = document.getElementById("generatorForm");   
+    const nameInput = document.getElementById("InputName");
+    const messageElement = document.getElementById("greeting");
+
+    formElement.addEventListener("submit", function(event) {           
+        event.preventDefault();                                        
+        const name = nameInput.value;                                  
+
+        if (name) {                                                     
+            messageElement.textContent = `Hello ${name}, I hope your day is going well!`;   
+        } else {
+            messageElement.textContent = "Please enter your name above.";
+
+            formElement.reset();                                           
+        }
+    });
+}); 
+
